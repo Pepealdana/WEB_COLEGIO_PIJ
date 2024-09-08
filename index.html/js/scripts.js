34327eb0177a2fe4db96572a9864e454b274f8b7
pepealdana <pepealdanagomez@hotmail.com>
2024-09-08 18:23:49 -0500

    Ajustes sidebar

--- index.html/js/scripts.js
+++ index.html/js/scripts.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById('menu-toggle');
     const nav = document.querySelector('.main-nav ul');
     const submenuItems = document.querySelectorAll('.submenu');
+    const hamburgerIcon = document.getElementById('hamburger-icon');
+    const sidebar = document.getElementById('sidebar');
+    const sidebarLinks = document.querySelectorAll('.sidebar a');
 
     // Toggle del menú hamburguesa
     menuToggle.addEventListener('click', function () {
@@ -23,9 +26,30 @@ document.addEventListener("DOMContentLoaded", function () {
             e.stopPropagation(); // Evitar que el evento burbujee hacia el menú principal
         });
     });
+
+    // Ocultar el sidebar al hacer clic en un enlace
+    sidebarLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            // Prevenir que el enlace realice su comportamiento por defecto
+            event.preventDefault();
+
+            // Ocultar el sidebar
+            sidebar.classList.remove('active');
+            hamburgerIcon.classList.remove('open');
+
+            // Si el enlace tiene un href con un ancla, realizar un scroll suave
+            const target = link.getAttribute('href');
+            if (target && target.startsWith('#')) {
+                const targetElement = document.querySelector(target);
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            }
+        });
+    });
 });
 
-// sección información
+// sección información (para el slider)
 document.addEventListener('DOMContentLoaded', () => {
     let currentSlide = 0;
     const slides = document.querySelectorAll('.slider-item');
